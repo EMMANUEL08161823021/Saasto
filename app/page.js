c1d45d18bd744ffe3603615d7dd391d9364920f6
EMMANUEL08161823021 <117604958+EMMANUEL08161823021@users.noreply.github.com>
2025-10-21 01:12:08 +0100

    removing tracker section

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
       <Hero/>
       <Features/>
       <Report/>
-      <Tracker/>
+      {/* <Tracker/> */}
       <Flexible/>
       <Trusted/>
       <Client/>
